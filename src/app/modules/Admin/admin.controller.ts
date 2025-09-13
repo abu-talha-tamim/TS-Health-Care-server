dea5fb977189c017f4c87c054620616677679b67
Abu Talha Tamim <your-github-email@example.com>
2025-09-13 16:07:16 +0600

    admin service search add

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -3,14 +3,23 @@ import{ Request, Response } from 'express';
 import { adminService } from './admin.service';
 
 const getAllFromDB = async (req: Request, res: Response) => {
-    const result = await adminService.getAllFromDB();
+    console.log(req.query);
+   try {
+     const result = await adminService.getAllFromDB(req.query);
         res.status(200).json({
             success: true,
             message: "Admin fetched successfully",
             data: result
         });
-    };
-    
-    export const adminController = {
-        getAllFromDB
-    };
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: (error as Error)?.name || "Something went wrong",
+            error: error
+        });
+    }
+};
+
+export const adminController = {
+    getAllFromDB
+};
